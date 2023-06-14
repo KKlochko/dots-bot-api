49f7f06e667c980fc8470305e3f19e8e346e553b
KKlochko <KKlochko@protonmail.com>
2023-06-14 13:46:56 +0300

    Added Dots Platform API Configuration.

---
+++ config/dotsapi.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Dots Platform API Configuration
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for the Dots Platform API.
+    | The file provides information about the keys to access the API.
+    |
+    */
+
+    'base_url' => env('DOTS_BASE_URL'),
+
+    'api_token' => env('DOTS_API_TOKEN'),
+
+    'api_account_token' => env('DOTS_API_ACCOUNT_TOKEN'),
+
+    'api_auth_token' => env('DOTS_API_AUTH_TOKEN'),
+
+];
+
