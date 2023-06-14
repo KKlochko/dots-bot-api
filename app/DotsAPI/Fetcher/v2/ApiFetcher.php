87483cce4e857402550a0854aafa074b205b556a
KKlochko <KKlochko@protonmail.com>
2023-06-14 17:46:04 +0300

    Added ApiFetcher to fetch data from DotsAPI.

---
+++ app/DotsAPI/Fetcher/v2/ApiFetcher.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DotsAPI\Fetcher\v2;
+
+use GuzzleHttp\Client;
+
+class ApiFetcher
+{
+    public function get($endpoint) {
+        $client = new Client();
+
+        $baseUrl = config('dotsapi.base_url');
+        $apiToken = config('dotsapi.api_token');
+        $apiAccountToken = config('dotsapi.api_account_token');
+
+        $apiUrl = $baseUrl . $endpoint;
+
+        $response = $client->get(
+            $apiUrl,
+            [
+                'headers' => [
+                    'Api-Token' => $apiToken,
+                    'Api-Account-Token' => $apiAccountToken,
+                    'Content-Type' => 'application/json',
+                    'Accept' => 'application/json',
+                ],
+                'query' => [
+                    'v'=> '2.0.0',
+                ],
+            ]
+        );
+
+        $data = json_decode($response->getBody()->getContents(), true);
+        $items = $data['items'];
+
+        return $items;
+    }
+}
