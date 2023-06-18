b71073d81d42030aa7ef3e1cc3a0e9ab596b4846
KKlochko <KKlochko@protonmail.com>
2023-06-18 17:38:58 +0300

    Update the CompanySeeder to have a second company.

--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -8,11 +8,19 @@ use App\Models\Company;
 
 class CompanySeeder extends Seeder
 {
-    protected $test_company = [
-        'uuid' => 'b7301b09-fc0c-4d0d-a556-ed70fc8e40f7',
-        'name' => 'testCompany',
-        'image' => null,
-        'description' => 'Pellentesque tristique imperdiet tortor.'
+    protected $test_companies = [
+        [
+            'uuid' => 'b7301b09-fc0c-4d0d-a556-ed70fc8e40f7',
+            'name' => 'testCompany',
+            'image' => null,
+            'description' => 'Pellentesque tristique imperdiet tortor.'
+        ],
+        [
+            'uuid' => 'b7301b09-fc0c-4d0d-a546-ed70fc8e40f7',
+            'name' => 'testCompany2',
+            'image' => null,
+            'description' => 'Curabitur vulputate vestibulum lorem.'
+        ],
     ];
 
     /**
@@ -20,6 +28,7 @@ class CompanySeeder extends Seeder
      */
     public function run(): void
     {
-        Company::factory()->create($this->test_company);
+        foreach($this->test_companies as $test_company)
+            Company::factory()->create($test_company);
     }
 }
