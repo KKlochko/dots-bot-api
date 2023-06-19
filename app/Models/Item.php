aedb4d5949f0c458a9e3e5f587eb68f9702deacc
KKlochko <KKlochko@protonmail.com>
2023-06-19 15:10:47 +0300

    Add the validation for Item that returns message for requests.

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -30,5 +30,26 @@ class Item extends Model
     {
         return $this->belongsToMany(Cart::class, 'carts_items', 'item_id', 'cart_id');
     }
+
+    public static function validate_with_name(string $name)
+    {
+        $name = $name ?? '';
+
+        if($name == '')
+            return [
+                'error' => 'The item name is empty, please, write the name!!!'
+            ];
+
+        $item = Item::where('name', $name)->first();
+
+        if(!$item)
+            return [
+                'error' => 'A item with the name does not exist!!!'
+            ];
+
+        return [
+            'ok' => 'A item with the name is valid.'
+        ];
+    }
 }
 
