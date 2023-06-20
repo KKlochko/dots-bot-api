78c197549de2769bdc047f2004cce97bcc212c02
KKlochko <KKlochko@protonmail.com>
2023-06-20 20:13:44 +0300

    Add a resource and a collection for show CartItem.

---
+++ app/Http/Resources/API/v2/CartItemResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources\API\v2;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CartItemResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'uuid' => $this->uuid,
+            'name' => $this->name,
+            'url' => $this->url,
+            'description' => $this->description,
+            'price' => $this->price,
+            'image' => $this->image,
+            'count' => $this->count,
+        ];
+    }
+}
