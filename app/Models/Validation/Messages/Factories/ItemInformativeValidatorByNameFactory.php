2b37a4c2e72e31d2afd576def4ef697cd64bcbdb
KKlochko <KKlochko@protonmail.com>
2023-08-31 13:02:33 +0300

    Refactor ValidatorByNameFactories of Company and Item models.

--- app/Models/Validation/Messages/Factories/ItemInformativeValidatorByNameFactory.php
+++ app/Models/Validation/Messages/Factories/ItemInformativeValidatorByNameFactory.php
@@ -2,45 +2,28 @@
 
 namespace App\Models\Validation\Messages\Factories;
 
-use App\Models\Validation\Messages\Factories\InformativeValidatorFactory;
+use App\Models\Validation\Messages\Factories\InformativeValidatorByNameFactory;
 use App\Models\Validation\Messages\InformativeValidator;
 
-use App\Models\Validation\Messages\OkInformativeValidator;
-use App\Models\Validation\Messages\NextInformativeValidator;
-use App\Models\Validation\Validators\EmptyNameValidator;
+use App\Models\Validation\Validators\Validator;
 use App\Models\Validation\Validators\ItemValidatorByName;
 
-class ItemInformativeValidatorByNameFactory extends InformativeValidatorFactory
+class ItemInformativeValidatorByNameFactory extends InformativeValidatorByNameFactory
 {
-    protected array $messages;
-    protected string $name;
-
     public function __construct(string $name)
     {
-        $this->messages = [
+        $this->setMessages([
             'found' => 'A item with the name is valid.',
             'not_found' => 'A item with the name does not exist!!!',
             'invalid_name' => 'The item name is empty, please, write the name!!!',
-        ];
+        ]);
 
-        $this->name = $name;
+        $this->setName($name);
     }
 
-    function create(): InformativeValidator
+    public function getValidatorByName(InformativeValidator $okValidator): Validator
     {
-        $okValidator = new OkInformativeValidator($this->messages['found']);
-
-        $nameValidator = new NextInformativeValidator(
-            $this->messages['not_found'],
-            new ItemValidatorByName($this->name, $okValidator),
-            $okValidator
-        );
-
-        return new NextInformativeValidator(
-            $this->messages['invalid_name'],
-            new EmptyNameValidator($this->name, $nameValidator),
-            $nameValidator
-        );
+        return new ItemValidatorByName($this->name, $okValidator);
     }
 }
 
